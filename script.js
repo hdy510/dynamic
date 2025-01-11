ac52e33ef341aca4d32f438b2481d0f72304ac21
hdy510 <hdy510@naver.com>
2025-01-11 23:57:03 +0900

    gsap 시간 조절 및 vw 적용

--- script.js
+++ script.js
@@ -2,94 +2,156 @@
 document.addEventListener("DOMContentLoaded", (event) => {
   gsap.registerPlugin(ScrollTrigger);
 
-  const tl = gsap.timeline({
+  // 1. 1스크롤에 블러 제품 이미지 확대
+  const tl1 = gsap.timeline({
     scrollTrigger: {
       trigger: ".conMain-products",
-      start: "top top",
-      end: "3000 bottom",
+      start: "0 top",
+      end: "600 bottom",
       scrub: 2,
       markers: true,
     },
   });
-  tl.to(".conMain-products", {
+  tl1.to(".conMain-products", {
     scale: 1,
     opacity: 1,
     filter: "blur(0px)",
   })
-    // .to(".conMain-ellipseBox", {
-    //   width: "200vw",
-    //   height: "200vh"
-    // })
-    .to(".conMain-ellipseBox2", {
-      width: "170vw",
-      height: "170vh",
-    })
-    .to(".conMain-text.beMore", {
-      right: 60,
-    }, "<")
-    .to(".conMain-text.dynamic", {
-      left: 60,
-    }, "<")
-    .to(".conMain-ellipseBox2", {
-      transform: "translate(-50%, -50%) rotate(0)"
-    });
+  
+  // 2. 1스크롤에 핑크타원, be more, dynamic 텍스트 등장 
+  const tl1a = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".conMain-products",
+      start: "600 top",
+      end: "1200 bottom",
+      scrub: 2,
+      markers: true,
+    },
+  });
+  tl1a.set(".conMain-ellipseBox", {
+    borderRadius: "50%",
+    rotate: "-15deg",
+    width: "10vw",
+    height: "5vw",
+  })
+  .to(".conMain-ellipseBox", {
+    width: "180vw",
+    height: "180vh",
+    opacity: 1,
+  })
+  .to(".conMain-text.beMore", {
+    right: "8vw",
+  }, "<")
+  .to(".conMain-text.dynamic", {
+    left: "8vw",
+  }, "<")
 
+  // 3. 1스크롤에 be more, dynamic 텍스트 사라짐, 핑크 납작해짐, 기기 사라짐
   const tl2 = gsap.timeline({
     scrollTrigger: {
       trigger: ".conMain-text",
-      start: "5000 top",
-      end: "7000 bottom",
+      start: "1600 top",
+      end: "5400 bottom",
       scrub: 2,
       markers: true,
     },
   });
-  tl2
-    .to(".conMain-text.beMore", {
-     right: "-100vw"
-    })
-    .to(".conMain-text.dynamic", {
-      left: "-100vw",
-    }, "<")
-    .fromTo(".conMain-ellipseBox2", {
-        height: "170vh",
-    }, {
-        height: 0,
-        transform: "translate(-50%, 0%) rotate(-15deg)"
-    }, "<")
-    .to(".conMain-products", {
-      filter: "blur(10px)",
-      transform: "translate(-50%, -65%) scale(1)",
-      opacity: 0,
-    }, "<70%")
-
+  tl2.fromTo(".conMain-ellipseBox", 
+    {
+      width: "180vw",
+      height: "180vh",
+      borderRadius: "50%",
+      rotate: "-15deg"
+    }, 
+    {
+      borderRadius: 0,
+      rotate: 0,
+      width: "100vw",
+      height: "100vh"
+    }
+  )
+  .to(".conMain-ellipseBox", {
+    scaleY: 0
+  })
+  .to(".conMain-text.beMore", {
+    right: "-100vw"
+  }, "<20%")
+  .to(".conMain-text.dynamic", {
+    left: "-100vw",
+  }, "<")
+  .to(".conMain-products", {
+    y: -100,
+    opacity: 0,
+  }, "<50%")
+  .fromTo(".conMain-products2", {
+    y: 70,
+    opacity: 0,
+  }, {
+    y: 0,
+    opacity: 1,
+  }, "<50%")
+    
+    
+  // 4. conMain-products2 제품 설명 텍스트 등장
+  var puffCounter = { var: 0 };
+  var count = document.getElementById('count');
   const tl3 = gsap.timeline({
     scrollTrigger: {
       trigger: ".conMain-products2",
-      start: "7000 top",
-      end: "12000 bottom",
+      start: "5400 top",
+      end: "8400 bottom",
       scrub: 2,
       markers: true,
     },
   });
-  tl3.to(".conMain-products2", {
+  tl3.fromTo(".conMain-text2", {
+    x: -100,
+    opacity: 0,
+  }, {
+    x: 0,
     opacity: 1,
   })
-  .to(".conMain-text2", {
+  .fromTo(".conMain-text3", {
+    x: 100,
+    opacity: 0,
+  }, {
+    x: 0,
     opacity: 1,
   })
-  .to(".conMain-text2", {
+  .to(puffCounter, {
+    var: 5000,
+    duration: 2,
+    onUpdate: function() {
+      count.innerHTML = Math.ceil(puffCounter.var);
+    },})
+  .fromTo(".conMain-text4", {
+    x: 100,
+    opacity: 0,
+  }, {
+    x: 0,
+    opacity: 1,
+  })
+
+  // 5. 이전 텍스트 사라지고 새로운 설명 텍스트 등장
+  const tl3a = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".conMain-products2",
+      start: "8400 top",
+      end: "11400 bottom",
+      scrub: 2,
+      markers: true,
+    },
+  });
+  tl3a.to(".conMain-text2", {
+    x: -100,
     opacity: 0,
   })
   .to(".conMain-text3", {
-    opacity: 1,
-  })
-  .to(".conMain-text4", {
-    opacity: 1,
-  })
-  .to(".conMain-text3", {
+    x: 100,
     opacity: 0,
   })
   .to(".conMain-text4", {
+    x: 100,
     opacity: 0,
   })
   .to(".conMain-products2", {
@@ -98,21 +160,33 @@ document.addEventListener("DOMContentLoaded", (event) => {
   .to(".conMain-products3", {
     opacity: 1,
   })
-  .to(".conMain-text5", {
+  .fromTo(".conMain-text5", {
+    x: -100,
+    opacity: 0,
+  }, {
+    x: 0,
     opacity: 1,
   })
-  .to(".conMain-text6", {
+  .fromTo(".conMain-text6", {
+    x: 100,
+    opacity: 0,
+  }, {
+    x: 0,
     opacity: 1,
   })
-  .to(".conMain-text5", {
-    opacity: 0,
-  })
-  .to(".conMain-text6", {
-    opacity: 0,
-  })
-  .to(".conMain-products3", {
-    opacity: 0,
-  })
+
+  // .to(".conMain-text6", {
+  //   opacity: 1,
+  // })
+  // .to(".conMain-text5", {
+  //   opacity: 0,
+  // })
+  // .to(".conMain-text6", {
+  //   opacity: 0,
+  // })
+  // .to(".conMain-products3", {
+  //   opacity: 0,
+  // })
 
   const tl4 = gsap.timeline({
     scrollTrigger: {
@@ -132,7 +206,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
   //   opacity: 0,
   //   transform: "scale(1)",
   // })
-  .to(".contentsWrap", {
+  .to(".conQuality-bg", {
     backgroundColor: "#FF560E",
   }, "<90%")
 
@@ -167,9 +241,12 @@ document.addEventListener("DOMContentLoaded", (event) => {
       markers: true,
     },
   });
-  tl6.to(".contentsWrap", {
+  tl6.to(".conQuality-bg", {
     backgroundColor: "#720EFF",
   }, "<90%")
+  .to(".conQuality-bestQuality", {
+    opacity: 0,
+  })
   .to(".conQuality-text2 img", {
     opacity: 1,
   })
@@ -192,7 +269,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
       markers: true,
     },
   });
-  tl7.to(".contentsWrap", {
+  tl7.to(".conQuality-bg", {
     backgroundColor: "#0E6EFF",
   }, "<90%")
   .to(".conQuality-text3 .detail.index0", {
@@ -221,10 +298,149 @@ document.addEventListener("DOMContentLoaded", (event) => {
     opacity: 1,
     stagger: 1,
   })
+  .to(".conQuality-text4 .detail", {
+    opacity: 1,
+  })
+
+  const tl9 = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".conQuality-experts",
+      start: "25500 top",
+      end: "28000 bottom",
+      scrub: 2,
+      markers: true,
+    },
+  });
+  tl9.set(".conQuality-bg", {
+    transformOrigin:"center center"
+  })
+  tl9.to(".conQuality-text4", {
+    opacity: 0,
+  })
+  .to(".conQuality-bg", {
+    borderRadius: "50%",
+    width: 150,
+    height: 150,
+  })
+  .to(".conQuality-bg", {
+    x: "-400",
+  })
+  .to(".conQuality-experts-text .detail.index0", {
+    opacity: 1,
+    x: "150",
+  })
+  .to(".conQuality-experts-text .detail.index1", {
+    opacity: 1,
+    x: "150",
+  })
+  .to(".conQuality-bg", {
+    opacity: 0,
+    x: "-500",
+  })
+  .to(".conQuality-experts", {
+    opacity: 0,
+    x: "-500",
+  }, "<")
+
+  const tl10 = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".conFlavor",
+      start: "28500 top",
+      end: "29000 bottom",
+      scrub: 2,
+      markers: true,
+    },
+  });
+  tl10.to(".conFlavor", {
+    opacity: 1,
+  })
+  .to(".conFlavor-description", {
+    x: 200,
+  }, "<")
+  .to(".conFlavor-imgs", {
+    x: -200,
+  }, "<")
+  .from(".conFlavor-title", {
+    y: -50,
+    opacity: 0,
+  }, "<")
+
+  const tl11 = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".conFlavor",
+      start: "30500 top",
+      end: "31000 bottom",
+      scrub: 2,
+      markers: true,
+    },
+  });
+  tl11.to(".conFlavor", {
+    opacity: 1,
+  })
+  .to(".conFlavor-description", {
+    x: -200,
+    opacity: 0,
+  }, "<")
+  .to(".conFlavor-imgs", {
+    x: 200,
+    opacity: 0,
+  }, "<")
+  .to(".conFlavor-title", {
+    y: -50,
+    opacity: 0,
+  }, "<")
+
+  const tl12 = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".conAboutUs",
+      start: "32000 top",
+      end: "38000 bottom",
+      scrub: 2,
+      markers: true,
+    },
+  });
+  tl12.to(".conAboutUs", {
+    opacity: 1,
+  })
+  .to(".conAboutUs-title", {
+    opacity: 1,
+  })
+  .to(".conAboutUs-descriptionBox img", {
+    opacity: 1,
+  })
+  .to(".conAboutUs-descriptionBox .introduce", {
+    opacity: 1,
+  })
+  .to(".conAboutUs-descriptionBox .promise", {
+    opacity: 1,
+  })
+  .to(".conAboutUs-descriptionBox button", {
+    opacity: 1,
+  })
 
 
 
 
+
+
+
+
+
+
+
+
+  // swiper 설정
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    slidesPerView: 4,
+    spaceBetween: 0,
+    loop: true,
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+    },
+  });
 
 
 
